1bf067cb0bd2c69a3ff704f6e36af5fb92bcb254
Nicholas Murray <nicholas.murray@live.com>
2023-03-16 06:02:38 +0000

    Spring Boot 3 and Spring Native To-Do

---
+++ spring3-todo-rest-demo/src/main/java/com/nmurray/spring3todorestdemo/data/repositories/ToDoItemRepoMock.java
@@ -0,0 +1,85 @@
+package com.nmurray.spring3todorestdemo.data.repositories;
+
+import com.nmurray.spring3todorestdemo.data.entities.ToDoItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Nicholas Murray
+ * A mock repository for testing without a connected MongoDB
+ */
+public class ToDoItemRepoMock {
+
+    /**
+     * A list to represent our database
+     */
+    private List<ToDoItem> repository = new ArrayList<>();
+
+    /**
+     * Default constructor
+     */
+    public ToDoItemRepoMock() {
+    }
+
+    /**
+     * Saves the given entity in the mock database
+     * @param newToDo - the new entity to be persisted
+     * @return - a copy of the saved entity
+     */
+    public ToDoItem save(ToDoItem newToDo) {
+        int index = -99;
+        //Check if the newToDo has an ID, if so check in the repository for
+        //an existing entity with the same ID
+        if (newToDo.getId() != null) {
+            index = findIdInRepo(newToDo.getId());
+        }
+
+        //If we found an existing entity with the same ID update it instead
+        //of creating a new one
+        if (index != -99) {
+            repository.get(index).setCompleted(newToDo.isCompleted());
+            repository.get(index).setDescription(newToDo.getDescription());
+            return repository.get(index);
+        } else {
+            Integer id = repository.size() + 1;
+            newToDo.setId(id.toString());
+            repository.add(newToDo);
+            return newToDo;
+        }
+    }
+
+    /**
+     * Returns the list of all entities from the mock database
+     * @return - all saved entities
+     */
+    public List<ToDoItem> findAll() {
+        return repository;
+    }
+
+    /**
+     * Deletes the entity with the given ID
+     * @param id - the ID of the entity to be deleted
+     * @throws IllegalArgumentException - when the given ID doesn't exist
+     */
+    public void deleteById(String id) {
+        int index = findIdInRepo(id);
+        if(index != -99) {
+            repository.remove(index);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Check in the repository for an entity with the given ID
+     */
+    private int findIdInRepo(String id) {
+        for( int i = 0; i <= repository.size(); i++) {
+            if(repository.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -99;
+    }
+}
---
+++ spring3-todo-rest-demo/src/test/java/com/nmurray/spring3todorestdemo/ToDoRestDemoUsingSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmurray.spring3todorestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoRestDemoUsingSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
